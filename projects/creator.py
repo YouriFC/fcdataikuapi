15b1c8e76f0bce012a1de4a830a92f64b366759c
YouriFC <youri.immerzeel@frieslandcampina.com>
2019-09-09 12:43:20 +0200

    First user check

--- projects/creator.py
+++ projects/creator.py
@@ -1,9 +1,24 @@
 import dataiku 
+import operator
 
-class Creator:
+class ProjectCreator:
+    '''
+    ProjectCreator is used by team leads to create pre-configured projects. 
+    :param gds_name: team lead's gds credentials. Used for verification & config mapping. 
+    :type gds_name: str
+    '''
 
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,gds_name):
+        self.client = dataiku.api_client()
+        self.gds_name = gds_name
+
+        gds_name = property(operator.attrgetter('_gds_name'))
+
+        @gds_name.setter
+        def gds_name(self, gdn):
+            userlist = self.client.list_users()
+            assert(gdn.lower() in [x['login'].lower() for x in userlist])
+            self._gds_name = gdn
 
     def create(self):
-        print(self.name)
+        print(self.gds_name)
