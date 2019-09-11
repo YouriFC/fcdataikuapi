4570a548b372034c466719b0b47f97fb86c48bde
YouriFC <youri.immerzeel@frieslandcampina.com>
2019-09-11 08:12:48 +0200

    Changed to ssh over https

--- projects/creator.py
+++ projects/creator.py
@@ -22,5 +22,49 @@ class ProjectCreator:
     def create(self, project_key, project_name):
         '''
         Create a new DataIKU project. 
+        :param project_key: The project key is used to reference datasets between projects. It cannot be changed once the project is created.
+        :param project_name: The display name of your dataiku project.
         '''
         new_project = self.__client.create_project(project_key, project_name, owner=self.gds_name)
+
+        #Ensure correct group access to project
+        #Intentionally hardcoded
+        project_permissions = new_project.get_permissions()
+        project_permissions['permissions'].append({
+                                                'group': self.devops_team,
+                                                'admin': False,
+                                                'exportDatasetsData': True,
+                                                'manageAdditionalDashboardUsers': False,
+                                                'manageDashboardAuthorizations': False,
+                                                'manageExposedElements': False,
+                                                'moderateDashboards': True,
+                                                'readDashboards': True,
+                                                'readProjectContent': True,
+                                                'runScenarios': True,
+                                                'writeDashboards': True,
+                                                'writeProjectContent': True
+                                            })
+        project_permissions['permissions'].append({
+                                                'group': self.devops_team + '_lead',
+                                                'admin': True,
+                                                'exportDatasetsData': True,
+                                                'manageAdditionalDashboardUsers': True,
+                                                'manageDashboardAuthorizations': True,
+                                                'manageExposedElements': True,
+                                                'moderateDashboards': True,
+                                                'readDashboards': True,
+                                                'readProjectContent': True,
+                                                'runScenarios': True,
+                                                'writeDashboards': True,
+                                                'writeProjectContent': True
+                                            })
+        new_project.set_permissions(project_permissions)                                    
+
+
+        ##TODO 
+
+
+        #Fix project code env in settings
+        #Fix selecting correct container
+        
+
