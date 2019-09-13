d15ec1af55e187fc2395c50666140cf569cab481
YouriFC <youri.immerzeel@frieslandcampina.com>
2019-09-13 08:53:12 +0200

    S3 connector V1

---
+++ connections/s3.py
@@ -0,0 +1,48 @@
+import dataiku 
+import re
+
+from ..validator.admin import AdminValidator
+
+class S3Creator:
+    '''
+    S3 is used by the platform admin to create pre-configured s3 connectors. 
+    :param gds_name: Admin gds credentials. 
+    :param devops_team: Just for checking purposes. 
+    '''
+
+    def __init__(self, gds_name, devops_team):
+        #client object for this class to use
+        self.__client = dataiku.api_client()
+
+        #Validate user is allowed to do this
+        self.gds_name, self.devops_team = AdminValidator(self.__client, gds_name, devops_team)
+        
+    
+    def create(self, target_devops_team):
+        '''
+        Create a new S3 connector. 
+        :param target_devops_team: The team for which a new S3 connection needs to be set up.
+        '''
+
+        #Quick Check
+        assert(target_devops_team.lower() in [x['name'].lower() for x in self.__client.list_groups()]), "Target devops team does not exist."
+
+        #Define standard parameter paylaod
+        payload = {'useDefaultCredentials': True,
+                'defaultManagedPath': '/dataiku',
+                'regionOrEndpoint': 'eu-west-1',
+                'hdfsInterface': 'S3A',
+                'encryptionMode': 'SSE_S3',
+                'chbucket': 'fcdatascience',
+                'chroot': target_devops_team,
+                'switchToRegionFromBucket': True,
+                'customAWSCredentialsProviderParams': [],
+                'namingRule': {}}
+        #Good to go
+
+        self.__client.create_connection(name='s3_' + target_devops_team,
+                                        type='EC2',
+                                        params=payload,
+                                        usable_by='ALLOWED',
+                                        allowed_groups=target_devops_team) 
+       
--- projects/creator.py
+++ projects/creator.py
@@ -7,7 +7,7 @@ class ProjectCreator:
     '''
     ProjectCreator is used by team leads to create pre-configured projects. 
     :param gds_name: Your gds credentials. Used for verification & config mapping. 
-    :param team: The team for which you'd like to create this projects. You must be team lead. For formatting, refer to DS DevOps teams in OneNote. 
+    :param devops_team: The team for which you'd like to create this projects. You must be team lead. For formatting, refer to DS DevOps teams in OneNote. 
     '''
 
     def __init__(self, gds_name, devops_team):
